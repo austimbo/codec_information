b7d3248c2f212589eae3d7754180f0dac6459172
tleach@cisco.com <tleach@cisco.com>
2019-11-07 11:29:38 +1100

    Version 2.0 Added Daniele's pretty print table

--- codec_info.py
+++ codec_info.py
@@ -4,12 +4,10 @@ import requests
 from requests.auth import HTTPBasicAuth
 from requests import Timeout
 from lxml import etree
+from prettytable import PrettyTable
 import sys
 
-userid="admin"
-password="cisco"
 headers={'content-type': 'text/xml'}
-#Device_IP="192.168.0.42"
 xml_get="/getxml?location="
 #Codec_URL="http://"+Device_IP
 network_xml_suffix="/status/network"
@@ -19,14 +17,16 @@ content_type= {'content-type' : 'text/xml'}
 codec_file="codecs.txt"
 codec_info_file="codec_info.csv"
 
+#Set up Prettytable parameters to draw an ASCII-based table
+MainSwTable = PrettyTable()
+
 #Here are the commands that we need t send to the device
 #xStatus Network 1 Ethernet MacAddress
 #xStatus SystemUnit Hardware Module SerialNumber !
 
 
-#Function to retreive at addrebut via HTTP GET
+#Function to retreive at information via a via HTTP GET
 #make a basic get to retreive the codec information
-#Retreive the network interface information
 def get_from_codec(URL,userid, password):
     auth = HTTPBasicAuth(userid, password)
     try:
@@ -48,16 +48,19 @@ def extract_value_from_xml(xml_text,xml_path):
 
 def request_comms_error_report(message):
     codec_info_f.write("%s, Error retreiving information,%s," % (Device_IP,message))
+    MainSwTable.add_row([format(Device_IP), format(message),format('Error'),format('Error'),format('Error')])
 
 
 # Main - Execute the main routing
 if __name__ == "__main__":
-  #Open an output file
+ print('Version2.0- Last Change Daniele, pretty print table')
+  #Define Field names in the Pretty table MainSwTable
+ MainSwTable.field_names = ['SYSTEM NAME', 'MAC ADDRESS', 'IP ADDRESS', 'SERIAL NUMBER', 'PRODUCT ID']
+  # Open an output file
  with open("codec_info.csv",'w+') as codec_info_f:
     #Write the Header row to the file. Thi is useful for Microspft Excel
     codec_info_f.write("System Name, IPv4_address, mac_address, serial_number, product_id\n")
     with open(codec_file) as codec_file_f:
-        #for Codec_IP in codec_file_f:
         for Codec_csv in codec_file_f:
             #Split record from the CSV file into three fields
             Device_IP,userid,password=Codec_csv.split(",")
@@ -67,10 +70,8 @@ if __name__ == "__main__":
             if response:
                 #Retreive the MacAddress tag
                 mac_address=extract_value_from_xml(response,"/Network/Ethernet/MacAddress")
-                print('Mac address is: {}'.format(mac_address.text))
                 #Retreive the IPV4 Network address
                 IPv4_address=extract_value_from_xml(response,"/Network/IPv4/Address")
-                print('IPv4 address is: {}'.format(IPv4_address.text))
             else:
                 request_comms_error_report('Network Stuff failed')
                 continue
@@ -79,10 +80,8 @@ if __name__ == "__main__":
             if response:
             #Retreive the serial number from the device
                 serial_number=extract_value_from_xml(response,"/Hardware/Module/SerialNumber")
-                print('Serial Number is: {}'.format(serial_number.text))
+            # Retreive the product ID from the XL Object
                 product_id=extract_value_from_xml(response,"/ProductId")
-                 #Retreive the product ID from the XL Object
-                print('Product ID is: {}'.format(product_id.text))
             else:
                 request_comms_error_report('Serial Number Stuff failed')
                 continue
@@ -90,13 +89,15 @@ if __name__ == "__main__":
             response = get_from_codec(Codec_URL + xml_get + user_interface_suffix, userid, password)
             if response:
                 system_name=extract_value_from_xml(response,"/UserInterface/ContactInfo/Name")
-                print('System Name is: {}'.format(system_name.text))
                 #Write the output to the CSV file
                 codec_info_f.write("%s,%s,%s,%s,%s\n" % (system_name.text,IPv4_address.text,mac_address.text,serial_number.text,product_id.text))
+                #Then write a record of all of the results to the PrettyTable
+                MainSwTable.add_row([format(system_name.text), format(mac_address.text), format(IPv4_address.text),format(serial_number.text), format(product_id.text)])
                 requests_comms_error = False
             else:
                 request_comms_error_report('User Interface Stuff failed')
                 continue
-
+    #As one last thing, print the prettyprint table
+    print(MainSwTable)
 
 
